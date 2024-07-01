ba8b9f2cb489b228e939b450180f3a861e2f4d90
Victor_yfZhao <13101539+victor-yfzhao@user.noreply.gitee.com>
2024-07-01 09:33:35 +0800

    Initial commit

---
+++ src/main/java/org/laorui_out/habit_former/HabitFormerApplication.java
@@ -0,0 +1,13 @@
+package org.laorui_out.habit_former;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitFormerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitFormerApplication.class, args);
+    }
+
+}
