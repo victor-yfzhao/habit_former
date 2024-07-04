36b1f6793fc91ab217b2f9a23b330d03a9c3c833
Victor_yfZhao <13101539+victor-yfzhao@user.noreply.gitee.com>
2024-07-04 09:29:26 +0800

    Init project's structure

--- src/main/java/org/laorui_out/habit_former/HabitFormerApplication.java
+++ src/main/java/org/laorui_out/habit_former/HabitFormerApplication.java
@@ -1,13 +1,13 @@
 package org.laorui_out.habit_former;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan
 @SpringBootApplication
 public class HabitFormerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HabitFormerApplication.class, args);
     }
-
 }
---
+++ src/main/java/org/laorui_out/habit_former/bean/UserBean.java
@@ -0,0 +1,17 @@
+package org.laorui_out.habit_former.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("user")
+public class UserBean {
+    @TableId(type = IdType.INPUT)
+    private String username;
+
+    @TableField("password")
+    private String password;
+}
