b0123548420f39ff39d687bd90d3019268a4d115
kkkc <2079939652@qq.com>
2024-07-06 11:51:50 +0800

    'fix:为plan相关实体添加了status字段'

--- src/main/java/org/laorui_out/habit_former/bean/PlanBean.java
+++ src/main/java/org/laorui_out/habit_former/bean/PlanBean.java
@@ -1,12 +1,16 @@
 package org.laorui_out.habit_former.bean;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class PlanBean {
+    @TableId(type = IdType.AUTO)
     private int planID;
     private String planName;
     private String planInfo;
+    private String status;
 
     @Override
     public String toString() {
@@ -14,6 +18,7 @@ public class PlanBean {
                 "planID=" + planID +
                 ", planName='" + planName + '\'' +
                 ", planInfo='" + planInfo + '\'' +
+                ", status='" + status + '\'' +
                 '}';
     }
 }
---
+++ src/main/java/org/laorui_out/habit_former/plan/constant/Constants.java
@@ -0,0 +1,10 @@
+package org.laorui_out.habit_former.plan.constant;
+
+public class Constants {
+    public static final String NOT_CHECKED="未打卡";
+    public static final String CHECKED="已打卡";
+    public static final String FAILED="过期";
+
+    //防止实例化
+    private Constants(){}
+}
