a093e49e18f58cc8b048b372cb718ee3419d0736
lewis <43459215@qq.com>
2024-07-10 15:12:59 +0800

    稍微更新了一些

--- src/main/java/org/laorui_out/habit_former/mapper/PlanMapper.java
+++ src/main/java/org/laorui_out/habit_former/mapper/PlanMapper.java
@@ -1,17 +1,44 @@
 package org.laorui_out.habit_former.mapper;
 
-import org.apache.ibatis.annotations.Select;
-
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.*;
+import org.laorui_out.habit_former.bean.PlanBean;
+import org.laorui_out.habit_former.plan.constant.Constants;
+import java.util.Date;
 import java.util.List;
 
-public interface PlanMapper {
+@Mapper
+public interface PlanMapper extends BaseMapper<PlanBean> {
+    //查询v2.0
+    @Select("select * from plan where userID=#{userID}")
+    List<PlanBean> getAllPlanByUserID(int userID);
+
+    @Select("select * from plan where planID=#{planID}")
+    PlanBean getPlanByPlanID(int planID);
+
+    @Select("select * from plan where planDate=#{date} AND userID=#{userID}")
+    List<PlanBean> getPlanByDate(Date date, int userID);
+
+    //插入v2.0
+    @Insert("insert " +
+            "into plan(planName,planInfo,status,userID,planDate,planTime,planType) " +
+            "values(#{planName},#{planInfo},'"+ Constants.NOT_CHECKED +"',#{userID},#{planDate},#{planTime},#{planType})")
+    @Options(useGeneratedKeys = true, keyProperty = "planID")
+    int addPlan(PlanBean planBean);
+
+    //更新v2.0
+    //date和time不允许更新
+    @Update("UPDATE plan " +
+            "SET planName = #{planName}, planInfo = #{planInfo}, status = #{status} " +
+            "WHERE planID = #{planID} ")
+    int updatePlan(PlanBean planBean);
+
+    //删除v2.0
+    @Delete("Delete from plan where planID=#{planID}")
+    int deletePlanByID(int planID);
+
+    @Delete("Delete from plan where userID=#{userID}")
+    int deleteAllPlanByUserID(int userID);
 
 
-
-
-
-
-
-    @Select("select planID from plan")
-    List<Integer> getAllPlanID();
 }
--- src/main/java/org/laorui_out/habit_former/poster/service/CreatePosterService.java
+++ src/main/java/org/laorui_out/habit_former/poster/service/CreatePosterService.java
@@ -4,6 +4,8 @@ import jakarta.annotation.Resource;
 import org.laorui_out.habit_former.bean.PosterBean;
 import org.laorui_out.habit_former.bean.PosterPictureBean;
 import org.laorui_out.habit_former.bean.UserBean;
+import org.laorui_out.habit_former.bean.PlanBean;
+import org.laorui_out.habit_former.mapper.PlanMapper;
 import org.laorui_out.habit_former.mapper.PosterMapper;
 import org.laorui_out.habit_former.mapper.UserMapper;
 import org.springframework.stereotype.Service;
@@ -17,11 +19,25 @@ public class CreatePosterService {
     PosterMapper posterMapper;
     @Resource
     UserMapper userMapper;
+    @Resource
+    PlanMapper planMapper;
+
 
     public String createPoster(int userID, int planID, String posterHeadline, List<String> posterPicture, String posterDetail){
         UserBean userTestBean = userMapper.getUserProfileThroughID(userID);
         if(userTestBean == null){
             return "不存在对应的用户，帖子创建失败";
+        }
+        PlanBean planTestBean = planMapper.getPlanByPlanID(planID);
+        if(planTestBean == null){
+            return "不存在对应的计划，帖子创建失败";
+        }
+
+        if(posterHeadline==null || posterHeadline.isEmpty()){
+            return "帖子标题不能为空";
+        }
+        if(posterDetail==null || posterDetail.isEmpty()){
+            return "帖子详情不能为空";
         }
 
         //判断计划存不存在！！！！
@@ -33,9 +49,9 @@ public class CreatePosterService {
         posterBean.setPlanID(planID);
         posterBean.setPosterDate(LocalDate.now());
         //获取当前的posterID,和userID一起插入关系表中
-        //更新一个poster的信息
+        //更新一个poster的信息s
         try{
-            if(posterPicture == null){
+            if(posterPicture == null || posterPicture.isEmpty()){
                 return "没有图片，帖子创建失败";
             }
             int isUP = posterMapper.insertPoster(posterBean);
